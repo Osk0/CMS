9ef72edab33b2f435bb993ddc175b4be13862740
Osk0 <oskarzebrzuski@gmail.com>
2024-02-26 13:29:13 +0100

    zmiany 26.02.2024

---
+++ upload.php
@@ -0,0 +1,61 @@
+<?php
+if(!empty($_POST)) {
+    $postTitle = $_POST['postTitle'];
+    $postDescription = $_POST['postDescription'];
+
+    $targetDirectory = "img/";
+    //$fileName = $_FILES['file']['name'];
+
+    $fileName = hash('sha256', $_FILES['file']['name'].microtime() );
+
+    $fileString = file_get_contents($_FILES['file']['tmp_name']);
+    //move_uploaded_file($_FILES['file']['tmp_name'], $targetDirectory.$fileName);
+
+    $gdImage = imagecreatefromstring($fileString);
+    $finalUrl = "http://localhost/cms/".$fileName.".webp";
+    $internalUrl = "img/".$fileName."webp";
+
+    imagewebp($gdImage, $internalUrl );
+
+    
+
+
+
+
+    $authorID = 1;
+ 
+
+    $db = new mysqli('localhost', 'root', '', 'cms');
+    $q = $db->prepare("INSERT INTO post (author, imgUrl, title) VALUES (?, ?, ?)");
+    $q->bind_param("iss", $authorID, $imageURL, $postTitle);
+    $q->execute();
+}
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dodaj nowy post</title>
+</head>
+<body>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <label for="postTitleInput">Tytuł posta: </label>
+        <input type="text" name="postTitle" id="postTitleInput">
+        <br>
+
+        <label for="postDescriptionInput">Opis posta:</label>
+        <input type="text" name="postDescription" id="postDescriptionInput">
+        <br>
+        <label for="fileInput">Obrazek: </label>
+        <input type="file" name="file" id="fileInput">
+        <br>
+        <input type="submit" value="Wyślij!">
+    </form>
+</body>
+</html>
